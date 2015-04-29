08f4f6df76f65ef25e96f3dd6bf2122b181f594b
Anton Simernia <anton.simernya@gmail.com>
2015-04-29 18:40:31 +0530

    Create table contact, install south and make migrations

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    date_of_birth = models.DateField()
+    bio = models.TextField()
+    email = models.EmailField()
+    jabber = models.CharField(max_length=200)
+    skype = models.CharField(max_length=200)
+    other_contacts = models.TextField()
